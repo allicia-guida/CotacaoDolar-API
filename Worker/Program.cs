af06843306352e02145e1dad5e759cea5d823809
Allicia Guida <alliciagd@gmail.com>
2026-04-23 12:46:02 -0300

    Worker configurado para atualização em tempo real da cotação do Dólar

--- Worker/Program.cs
+++ Worker/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+
+var url = "https://economia.awesomeapi.com.br/json/last/USD-BRL";
+
+using var httpClient = new HttpClient();
+
+try
+{
+    var response = await httpClient.GetStringAsync(url);
+    using JsonDocument doc = JsonDocument.Parse(response);
+    var usdBrL = doc.RootElement.GetProperty("USDBRL");
+    var valor = usdBrL.GetProperty("bid").GetString();
+    var data = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+    var resultado = new
+    {
+        valor = valor,
+        data = data
+    };
+    var json = JsonSerializer.Serialize(resultado, new JsonSerializerOptions 
+    {
+        WriteIndented = true 
+    });
+
+    var caminhoArquivo = Path.Combine("..", "Api", "data.json");
+    File.WriteAllText(caminhoArquivo, json);
+    Console.WriteLine("Cotação atualizada com sucesso!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro ao buscar cotação: {ex.Message}");
+}
